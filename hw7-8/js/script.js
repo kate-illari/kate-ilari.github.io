adf113b51b7cb8e84e6ee76f978e1336634c5988
Emily <kate.illari@gmail.com>
2017-06-09 03:09:46 +0300

    oh i navayala

--- hw7-8/js/script.js
+++ hw7-8/js/script.js
@@ -1,20 +1,55 @@
 (function ($, undefined) {
+    'use strict';
     $(function () {
 
-        var tabs = $('.tabs');
-        var articles = $('article');
-        console.log(articles);
+        var $tabs = $('.tabs'),
+            $articles = $('article'),
+            $inputs = $('input'),
+            $tooltips = $('.tooltips');
 
-        articles.hide().first().show();
-        tabs.first().addClass('activeTab');
+        $articles.hide().first().show();
+        $tabs.first().addClass('activeTab');
+        $tooltips.hide();
 
-        tabs.click(function () {
-            articles.hide();
-            tabs.removeClass();
-            $(this).addClass('activeTab');
+//tabs events
+
+        $tabs.click(function () {
             var articleID = $(this).data('image');
+            $articles.hide();
+            $tabs.removeClass("activeTab hover");
+            $(this).addClass("activeTab");
             $(articleID).show();
         });
 
+
+        $tabs.hover(function () {
+            if (!$(this).hasClass("activeTab")) {
+                $(this).addClass("hover");
+            }
+        }, function () {
+            $(this).removeClass("hover");
+        });
+
+//form events
+
+        $inputs.hover(function () {
+            var tooltipID = $(this).data('input');
+            $tooltips.hide();
+            $(tooltipID).show();
+        }, function () {
+            $tooltips.hide();
+            $('#formButt').removeClass("tipsShown");
+        });
+
+        $('#formButt').click(function () {
+            if (!$(this).hasClass("tipsShown")) {
+                $(this).addClass("tipsShown");
+                $tooltips.show();
+            } else {
+                $(this).removeClass("tipsShown");
+                $tooltips.hide();
+            }
+        });
+
     });
-}(jQuery));
+})(jQuery);
