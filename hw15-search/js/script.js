13e97e1aa8ae485b94c3e6880906c024ef847804
Emily <kate.illari@gmail.com>
2017-07-11 17:20:26 +0300

    some changes to script.js

--- hw15-search/js/script.js
+++ hw15-search/js/script.js
@@ -13,24 +13,19 @@
         var container = document.getElementById('container');
         var fetchText = 'https://pixabay.com/api/?key=5837779-ac3ba737206b541ae294f1119&image_type=photo&q=' + inputValue;
 
-        while (container.hasChildNodes()) {
-            container.removeChild(container.lastChild);
-        }
+        container.innerHTML = '';
 
-        var promise = fetch(fetchText)
+        fetch(fetchText)
             .then(function (response) {
-                if (response.ok){
+                if (response.ok) {
                     return response.json();
                 }
-                throw new Error ('Error fetching data');
+                throw new Error('Error fetching data');
             })
             .then(function (data) {
                 if (data.hits.length > 0) {
                     data.hits.forEach(function (hits) {
-                        var img = document.createElement('img');
-
-                        container.appendChild(img);
-                        img.setAttribute('src', hits.webformatURL);
+                        container.innerHTML += '<img src="'+hits.webformatURL+'">';
                     });
                 } else {
                     container.innerHTML = 'No search result, please try something else';
@@ -43,7 +38,9 @@
 
     button.addEventListener('click', myContent);
     myInput.addEventListener('keyup', function (event) {
-        if (event.keyCode === 13) return myContent();
+        if (event.keyCode === 13) {
+            myContent();
+        }
     });
 
 
