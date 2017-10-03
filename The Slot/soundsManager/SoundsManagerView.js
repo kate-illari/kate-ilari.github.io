726240b8bb231f17753c818e08ff22448c34633a
Emily <kate.illari@gmail.com>
2017-10-03 18:13:43 +0300

    added/changed some sounds

--- The Slot/soundsManager/SoundsManagerView.js
+++ The Slot/soundsManager/SoundsManagerView.js
@@ -7,9 +7,50 @@ SoundsManagerView.prototype = {
   constructor: SoundsManagerView,
 
   init: function () {
+    var me = this;
 
+    me.backgroundSound = new Audio('audio/Background.mp3');
+    me.spinStartSound = new Audio('audio/StartSpin.mp3');
+    me.betChangeSound = new Audio('audio/pop.mp3');
+    me.reelStopSound = new Audio('audio/reelStop.mp3');
+    me.winSound = new Audio('audio/Win.mp3');
+    me.bigWinSound = new Audio('audio/BigWin.mp3');
+
+    me.playBgSound();
+  },
+
+  playBgSound: function () {
+    var me = this;
+
+    me.backgroundSound.volume = 0.4;
+    me.backgroundSound.loop = true;
+    me.backgroundSound.play();
+  },
+
+  playSpinStartSound: function () {
+    this.spinStartSound.play();
+  },
+
+  playBetChangeSound: function () {
+    var betChangeSoundClone = this.betChangeSound.cloneNode();
+    betChangeSoundClone.play();
+  },
+
+  playReelStopSound: function () {
+    var reelStopSoundClone = this.reelStopSound.cloneNode();
+    reelStopSoundClone.play();
+  },
+
+  playWinSound: function () {
+    this.winSound.volume = 0.5;
+    this.winSound.play();
+  },
+
+  playBigWinSound: function () {
+    this.winSound.volume = 0.7;
+    this.bigWinSound.play();
   }
-  
+
 };
 
 SoundsManagerView = utils.extend(View, SoundsManagerView);
